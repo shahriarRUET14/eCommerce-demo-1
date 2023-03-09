4b06d02124869a12473bc4e1845f62634889009e
mahmud <shahriar.ruet14@gmail.com>
2023-03-10 00:40:26 +0600

    Payment Page

--- utils/Store.js
+++ utils/Store.js
@@ -6,7 +6,7 @@ export const Store = createContext();
 const initialState = {
   cart: Cookies.get("cart")
     ? JSON.parse(Cookies.get("cart"))
-    : { cartItems: [], shippingAddress: {} },
+    : { cartItems: [], shippingAddress: {}, paymentMethod: "" },
 };
 
 function reducerFunction(state, action) {
@@ -55,6 +55,14 @@ function reducerFunction(state, action) {
         },
       };
 
+    case "SAVE_PAYMENT_METHOD":
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          paymentMethod: action.payload,
+        },
+      };
     default:
       return state;
   }
