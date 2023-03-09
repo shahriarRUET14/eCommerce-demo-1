aed5aa934e1db8280abdb6c4076818786fd99e4d
mahmud <shahriar.ruet14@gmail.com>
2023-03-09 22:06:48 +0600

    shipping address added

--- pages/shipping.js
+++ pages/shipping.js
@@ -1,20 +1,24 @@
-import Cookies from "js-cookie";
 import React, { useContext, useEffect } from "react";
 import { useForm } from "react-hook-form";
+import Cookies from "js-cookie";
 import CheckoutWizard from "../components/CheckoutWizard";
 import Layout from "../components/Layout";
 import { Store } from "../utils/Store";
+import { useRouter } from "next/router";
+
 export default function ShippingScreen() {
   const {
     handleSubmit,
     register,
     formState: { errors },
     setValue,
-    getValues,
   } = useForm();
+
   const { state, dispatch } = useContext(Store);
   const { cart } = state;
   const { shippingAddress } = cart;
+  const router = useRouter();
+
   useEffect(() => {
     setValue("fullName", shippingAddress.fullName);
     setValue("address", shippingAddress.address);
@@ -22,6 +26,7 @@ export default function ShippingScreen() {
     setValue("postalCode", shippingAddress.postalCode);
     setValue("country", shippingAddress.country);
   }, [setValue, shippingAddress]);
+
   const submitHandler = ({ fullName, address, city, postalCode, country }) => {
     dispatch({
       type: "SAVE_SHIPPING_ADDRESS",
@@ -31,28 +36,37 @@ export default function ShippingScreen() {
       "cart",
       JSON.stringify({
         ...cart,
-        shippingAddress: { fullName, address, city, postalCode, country },
+        shippingAddress: {
+          fullName,
+          address,
+          city,
+          postalCode,
+          country,
+        },
       })
     );
+
+    router.push("/payment");
   };
 
   return (
     <Layout title="Shipping Address">
-      <CheckoutWizard activeStep={1}></CheckoutWizard>
+      <CheckoutWizard activeStep={1} />
       <form
         className="mx-auto max-w-screen-md"
         onSubmit={handleSubmit(submitHandler)}
       >
         <h1 className="mb-4 text-xl">Shipping Address</h1>
-
         <div className="mb-4">
           <label htmlFor="fullName">Full Name</label>
           <input
-            id="fullName"
             className="w-full"
+            id="fullName"
             autoFocus
-            {...register("fullName", { required: "Please enter full name" })}
-          ></input>
+            {...register("fullName", {
+              required: "Please enter full name",
+            })}
+          />
           {errors.fullName && (
             <div className="text-red-500">{errors.fullName.message}</div>
           )}
@@ -60,17 +74,13 @@ export default function ShippingScreen() {
         <div className="mb-4">
           <label htmlFor="address">Address</label>
           <input
-            id="address"
             className="w-full"
-            autoFocus
+            id="address"
             {...register("address", {
-              required: "Please enter Address",
-              minLength: {
-                value: 3,
-                message: "Address is more than 3 characters long",
-              },
+              required: "Please enter address",
+              minLength: { value: 3, message: "Address is more than 2 chars" },
             })}
-          ></input>
+          />
           {errors.address && (
             <div className="text-red-500">{errors.address.message}</div>
           )}
@@ -78,40 +88,44 @@ export default function ShippingScreen() {
         <div className="mb-4">
           <label htmlFor="city">City</label>
           <input
-            id="city"
             className="w-full"
-            autoFocus
-            {...register("city", { required: "Please enter City" })}
-          ></input>
+            id="city"
+            {...register("city", {
+              required: "Please enter city",
+            })}
+          />
           {errors.city && (
-            <div className="text-red-500">{errors.city.message}</div>
+            <div className="text-red-500 ">{errors.city.message}</div>
           )}
         </div>
         <div className="mb-4">
           <label htmlFor="postalCode">Postal Code</label>
           <input
-            id="postalCode"
             className="w-full"
-            autoFocus
+            id="postalCode"
             {...register("postalCode", {
-              required: "Please enter Postal Code",
+              required: "Please enter postal code",
             })}
-          ></input>
+          />
           {errors.postalCode && (
-            <div className="text-red-500">{errors.postalCode.message}</div>
+            <div className="text-red-500 ">{errors.postalCode.message}</div>
           )}
         </div>
         <div className="mb-4">
-          <label htmlFor="country">Coutnry</label>
+          <label htmlFor="country">Country</label>
           <input
-            id="country"
             className="w-full"
-            autoFocus
-            {...register("country", { required: "Please enter Coutnry" })}
-          ></input>
+            id="country"
+            {...register("country", {
+              required: "Please enter country",
+            })}
+          />
           {errors.country && (
-            <div className="text-red-500">{errors.country.message}</div>
+            <div className="text-red-500 ">{errors.country.message}</div>
           )}
+        </div>
+        <div className="mb-4 flex justify-between">
+          <button className="primary-button">Next</button>
         </div>
       </form>
     </Layout>
